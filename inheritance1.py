86dd1f75dea3a7a889570f47ed612124ea875959
tayostyles <93762036+tayostyles@users.noreply.github.com>
2021-11-05 10:26:34 +0100

    Add files via upload

---
+++ inheritance1.py
@@ -0,0 +1,26 @@
+class Polygon:
+    def __init__(self, no_of_sides):
+        self.n = no_of_sides
+        self.sides = []
+        for i in range(no_of_sides):
+            self.sides.append(0)
+
+    def input_sides(self):
+        for i in range(self.n):
+            self.sides[i] = float(input("Enter side"+str(i+1) +" : "))
+
+# Inheriting Triangle from Polygon class
+
+class Triangle(Polygon):
+    def __init__(self):
+        Polygon.__init__(self , 3)
+
+    def find_area(self):
+        a, b, c = self.sides
+        # Calculate the semi-perimeter
+        s = (a + b + c) / 2
+        area = (s*(s-a)*(s-b)*(s-c)) ** 0.5
+        print("The area of triangle is" , area)
+t = Triangle()
+t.input_sides()
+t.find_area()
